659a706bb99f60cdb82188ff422edd49ef2bcdc4
Rafael Rodrigues <rafa.neg@live.com>
2019-10-03 00:08:46 -0300

    modificado o arquivo funcoes.py

--- Solutions-Python/Numbers/Fibonacci Sequence/fibonacci.py
+++ Solutions-Python/Numbers/Fibonacci Sequence/fibonacci.py
@@ -1,12 +1,12 @@
-import funções
+import funcoes
 while True:
-    escolha = funções.menu()
+    escolha = funcoes.menu()
     if escolha == 1:
-        funções.fibo_seq_start(funções.ler_int(input('Valor de inicio: ')))
+        funcoes.fibo_seq_start(funcoes.ler_int(input('Valor de inicio: ')))
     elif escolha == 2:
-        funções.fibo_seq_terms(funções.ler_int(input('Quantos termos: ')))
+        funcoes.fibo_seq_terms(funcoes.ler_int(input('Quantos termos: ')))
     else:
-        funções.fibo_seq_till(funções.ler_int(input('Máximo: ')))
+        funcoes.fibo_seq_till(funcoes.ler_int(input('Máximo: ')))
     cont = input('Desesa continuar(S/N)?')
     if cont not in 'Ss':
         break
